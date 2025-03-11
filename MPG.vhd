32f64add384971b451835cd2c1fea0f43f43e655
larisa229 <larisapasca29@gmail.com>
2025-03-11 19:27:13 +0200

    Add files via upload

--- MPG.vhd
+++ MPG.vhd
@@ -7,26 +7,50 @@ use ieee.std_logic_unsigned.all;
 entity MPG is
   Port (   clk : in STD_LOGIC;
            btn : in  std_logic_vector(4  downto 0);
-           sw  : in  std_logic_vector(15 downto 0);
-           led : out std_logic_vector(15 downto 0);
-           an  : out std_logic_vector(7  downto 0);
-           cat : out std_logic_vector(6  downto 0)
+           enable : out std_logic_vector(4  downto 0)
+
   );
 end MPG;
 
 architecture Behavioral of MPG is
 
 signal cnt: STD_LOGIC_VECTOR(15 downto 0):= (others => '0');
-signal d1, d2, d3, q1, q2, q3: STD_LOGIC;
+signal en: STD_LOGIC := '0';
+signal q1, q2, q3: STD_LOGIC_VECTOR(4 downto 0) := (others =>'0');
 
 begin
-    process (clk)
+    counter: process (clk)
     begin
         if rising_edge(clk) then
-          if btn(0) = '1' then 
             cnt <= cnt + 1;
-          end if;
+        end if;
+    end process counter;
+    
+    en <= '1' when cnt = x"000F" else '0';
+    
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            if en = '1' then
+                q1 <= btn;
+            end if;
         end if;
     end process;
+    
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            q2 <= q1;
+        end if;
+    end process;
+    
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            q3 <= q2;
+         end if;
+    end process;
+    
+    enable <= q2 and (not q3);
 
 end Behavioral;
