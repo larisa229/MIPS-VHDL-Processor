ce57993084b193ec4f07535473de5e2d518ff4a9
larisa229 <larisapasca29@gmail.com>
2025-03-14 15:49:06 +0200

    Add files via upload
    ALU

--- counter_decoder.vhd
+++ counter_decoder.vhd
@@ -17,8 +17,10 @@ end counter_decoder;
 architecture Behavioral of counter_decoder is
 
 signal en : STD_LOGIC_VECTOR(4 downto 0) := (others => '0');
-signal cnt : STD_LOGIC_VECTOR(15 downto 0) := (others => '0');
+signal cnt : STD_LOGIC_VECTOR(1 downto 0) := (others => '0');
 signal s_digits : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
+signal in1, in2, in3 : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
+signal out1, out2, out3, out4 : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
 
 component MPG is
   Port (   clk : in STD_LOGIC;
@@ -51,6 +53,37 @@ begin
        
     end process;
 
-    led <= cnt;
-    s_digits <= x"0000"&cnt;
+    led(1 downto 0) <= cnt;
+    in1 <= (31 downto 8 => '0')&sw(7 downto 0);
+    in2 <= (31 downto 8 => '0')&sw(15 downto 8);
+    in3 <= (31 downto 16 => '0')&sw(15 downto 0);
+    
+    process (in1, in2) 
+    begin
+        out1 <= in1 + in2;
+    end process;
+    
+    process (in1, in2) 
+    begin
+        out2 <= in1 - in2;
+    end process;
+    
+    process (in3) 
+    begin
+        out3 <= in3(29 downto 0)&"00";
+        out4 <= "00"&in3(31 downto 2);
+    end process;
+    
+    process (cnt)
+    begin
+      case cnt is
+        when "00"  => s_digits <= out1;
+        when "01"  => s_digits <= out2;
+        when "10"  => s_digits <= out3;
+        when others => s_digits <= out4;
+      end case;
+    end process;
+    
+    led(7) <= '1' when s_digits = x"00000000" else '0';
+   
 end Behavioral;
