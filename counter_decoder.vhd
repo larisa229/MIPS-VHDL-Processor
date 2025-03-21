7d5f4cf4b1a9dfe9d45af95694d25135c6c16f1d
Larisa Pașca <larisapasca29@gmail.com>
2025-03-21 15:09:43 +0200

    Add files via upload
    Register file implementation

--- counter_decoder.vhd
+++ counter_decoder.vhd
@@ -1,10 +1,9 @@
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
 
-entity counter_decoder is
+entity register_file is
     Port ( clk : in STD_LOGIC;
            btn : in  std_logic_vector(4  downto 0);
            sw  : in  std_logic_vector(15 downto 0);
@@ -12,15 +11,14 @@ entity counter_decoder is
            an  : out std_logic_vector(7  downto 0);
            cat : out std_logic_vector(6  downto 0)
            );
-end counter_decoder;
+end register_file;
 
-architecture Behavioral of counter_decoder is
+architecture Behavioral of register_file is
 
 signal en : STD_LOGIC_VECTOR(4 downto 0) := (others => '0');
-signal cnt : STD_LOGIC_VECTOR(1 downto 0) := (others => '0');
+signal cnt : STD_LOGIC_VECTOR(3 downto 0) := (others => '0');
 signal s_digits : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
-signal in1, in2, in3 : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
-signal out1, out2, out3, out4 : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
+signal s_rd1, s_rd2 : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
 
 component MPG is
   Port (   clk : in STD_LOGIC;
@@ -38,9 +36,23 @@ component seven_seg_disp is
   );
 end component;
 
+component reg_file is
+     Port ( clk : in std_logic;
+            ra1 : in std_logic_vector (3 downto 0);
+            ra2 : in std_logic_vector (3 downto 0);
+            wa : in std_logic_vector (3 downto 0);
+            wd : in std_logic_vector (31 downto 0);
+            wen : in std_logic;
+            rd1 : out std_logic_vector (31 downto 0);
+            rd2 : out std_logic_vector (31 downto 0)
+            );
+end component;
+
 begin
     debounce: MPG port map (clk => clk, btn => btn, enable => en);
     display : seven_seg_disp port map (clk => clk, digits => s_digits, an => an, cat => cat);
+    register_file: reg_file port map (clk => clk, ra1 => cnt, ra2 => cnt, wa => cnt, wd => s_digits, wen => en(3), rd1 => s_rd1, rd2 => s_rd2);
+  
     process(clk, en)
     begin
         if rising_edge(clk) then
@@ -52,38 +64,12 @@ begin
         end if;
        
     end process;
+    
+    process(s_rd1, s_rd2)
+    begin
+        s_digits <= s_rd1 + s_rd2;
+    end process;
 
-    led(1 downto 0) <= cnt;
-    in1 <= (31 downto 8 => '0')&sw(7 downto 0);
-    in2 <= (31 downto 8 => '0')&sw(15 downto 8);
-    in3 <= (31 downto 16 => '0')&sw(15 downto 0);
-    
-    process (in1, in2) 
-    begin
-        out1 <= in1 + in2;
-    end process;
-    
-    process (in1, in2) 
-    begin
-        out2 <= in1 - in2;
-    end process;
-    
-    process (in3) 
-    begin
-        out3 <= in3(29 downto 0)&"00";
-        out4 <= "00"&in3(31 downto 2);
-    end process;
-    
-    process (cnt)
-    begin
-      case cnt is
-        when "00"  => s_digits <= out1;
-        when "01"  => s_digits <= out2;
-        when "10"  => s_digits <= out3;
-        when others => s_digits <= out4;
-      end case;
-    end process;
-    
-    led(7) <= '1' when s_digits = x"00000000" else '0';
+    led <= x"000" & cnt;
    
 end Behavioral;
